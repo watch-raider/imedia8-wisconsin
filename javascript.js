44981982beaa00ef18c5be8938c32befcb440e68
watch-raider <07waltonmi@gmail.com>
2017-06-02 23:57:49 +0100

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,17 @@
+$('.lists').hide();
+
+$(document).ready(function() {
+  $('li').hover(function() {
+    $(this).toggleClass('touch');
+  })
+  $('.effect').hover(function() {
+    $(this).toggleClass('touch');
+  })
+  $('.sections').hover(function() {
+    $(this).toggleClass('underline');
+  })
+  $('.tabs').click(function() {
+    $('.lists').hide();
+    $(this).prev('.lists').show();
+  })
+});
